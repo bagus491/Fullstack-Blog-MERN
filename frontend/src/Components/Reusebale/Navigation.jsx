75ae9b5f6d4569be0582983ecb19f5ec8fbc00ce
bagus491 <baguskalam491@gmail.com>
2023-07-29 16:48:27 +0700

    setup&add (homePages dan animation Loading) => frontend

--- frontend/src/Components/Reusebale/Navigation.jsx
+++ frontend/src/Components/Reusebale/Navigation.jsx
@@ -4,20 +4,22 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import '../../Sass/main.css'
+
 export const Navigation = () => {
     return(
         <>
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#">YoursBlog</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Brand href="#" style={{fontFamily: 'cursive',color: 'white',fontSize: '25px'}}>YoursBlog</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll"  style={{background: 'white'}}/>
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>          
+            <Nav.Link href="#action1" className='text-white'>Home</Nav.Link>          
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -28,6 +30,7 @@ export const Navigation = () => {
             />
             <Button variant="outline-success">Search</Button>
           </Form>
+            <Button className='CreateAccountButton' variant='light'>CreateAccount</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- frontend/src/Pages/HomePages.jsx
+++ frontend/src/Pages/HomePages.jsx
@@ -1,9 +1,38 @@
+
+import { useEffect, useState } from "react"
 import { Navigation } from "../Components/Reusebale/Navigation"
+// import { Spinner} from 'react-bootstrap'
+import '../Sass/main.css'
+
 
 export const HomePages = () => {
+    const [getContent , setgetContent] = useState(true)
+    useEffect(() => {
+        setTimeout(() => {
+            setgetContent(false)
+        },4000)
+    },[])
+
     return(
         <div>
-            <Navigation />
+            <div className='mybghomedark'>
+                {
+                    getContent ? 
+                    <div className="spinner-animation">
+                       <div className="box-animation">
+                        <div className="text-box-animation">
+                            <h1>Loading</h1>
+                        </div>
+                       </div>
+                    </div>
+                    : 
+                    <div>
+                        <Navigation  />
+                    </div>
+                    
+                }
+
+            </div>
         </div>
     )
 }
