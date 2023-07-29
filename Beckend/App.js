e6a75c7547cf9708882a77a445ed23654eafc8f8
bagus491 <baguskalam491@gmail.com>
2023-07-29 13:27:15 +0700

    setup(middleware) => beckend App.js

--- Beckend/App.js
+++ Beckend/App.js
@@ -4,8 +4,19 @@ const app = express()
 const port = 5000
 
 
+//setup middleware ==> seconds
+const cors = require('cors')
+app.use(cors())
 
+const bodyparser = require('body-parser')
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json())
 
+const cookieparser = require('cookie-parser')
+app.use(cookieparser('secret'))
+
+const morgan = require('morgan')
+app.use(morgan('dev'))
 
 
 
