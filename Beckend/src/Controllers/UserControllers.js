ae7c69f94f2a60c318553ff68e3462593ffd365b
bagus491 <baguskalam491@gmail.com>
2023-07-29 22:50:33 +0700

    debugger&add&changes(LoginPages,Logicompo) ==> debugger Beckend UserControllers LoginPage forget give return before rest

--- Beckend/src/Controllers/UserControllers.js
+++ Beckend/src/Controllers/UserControllers.js
@@ -1,5 +1,8 @@
+// middleware
+const jwt = require('jsonwebtoken')
+const secret = '!@#%$^&daqwerg!@234551'
 
-
+const {CheckUser} = require('../Utils/Index')
 
 //first
 const HomeWeb = (req,res) => {
@@ -19,14 +22,31 @@ const HomeSearch = (req,res) => {
     }
 }
 
-//third
-const LoginPages = (req,res) => {
+//third --edited
+const LoginPages = async (req,res) => {
     try{
-        res.send('hello world login')
-    }catch{
-        res.send('hello world')
+    const token = req.headers.authorization
+    if(!token){
+        return res.status(401).json({msg: 'Not Authorization'})
     }
-}
+        jwt.verify(token,secret,async (err,decoded) => {
+          if(err){
+              return res.status(401).json({msg: 'Not Authorization'})
+          }
+          
+          const Username = decoded.Username
+          const dataOk = await CheckUser(Username)
+          if(!dataOk){
+              return res.status(401).json({msg : 'Not Authorization'})
+          }
+  
+          res.status(200).json({msg: 'Valid',Username})
+        })
+      }catch(error){
+              res.status(500).json({msg : 'Internal Server Error'})
+          }
+    };
+
 
 
 
--- frontend/src/Pages/LoginPages.jsx
+++ frontend/src/Pages/LoginPages.jsx
@@ -3,15 +3,41 @@ import { useEffect, useState } from "react"
 import { Spinner } from "react-bootstrap"
 import '../Sass/main.css'
 import { CardLogin } from "../Components/Sub-Compo/CardLogin"
+import {useNavigate} from 'react-router-dom'
 
 export const LoginPages = () => {
     const [getSpinner , setSpinner] = useState(true)
+    const getToken = localStorage.getItem('uL_')
+    const Navigate = useNavigate()
 
     useEffect(() => {
-        setTimeout(() => {
-            setSpinner(false)
-        },1000)
-    },[])
+       const getData = async () => {
+        try{
+            const respone = await fetch('http://localhost:5000/login', {
+                method: 'GET',
+                headers: {
+                    'Authorization' : getToken
+                }
+            })
+            if(respone.ok){
+                const json = await respone.json()
+              if(respone.status === 200) {
+                Navigate(`/dasbord/${json.Username}`)
+              }
+            }else {
+                setTimeout(() => {
+                    setSpinner(false)
+                },1000)
+            }
+             
+        }catch{
+            console.error({msg : 'Error'})
+        }
+       }
+       getData()
+    },[getToken,Navigate])
+
+
     return(
         <div>
            {
