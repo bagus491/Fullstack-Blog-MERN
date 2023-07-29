8d7c6b0cf524bc46bf9e525c24e3b5e318dae89c
bagus491 <baguskalam491@gmail.com>
2023-07-29 21:50:34 +0700

    add(Balancer Token) => frontend Dasbord Pages

--- frontend/src/Pages/DasbordPages.jsx
+++ frontend/src/Pages/DasbordPages.jsx
@@ -1,11 +1,25 @@
 
-
-
+import { AuthContext } from "../AuthContext"
+import { useContext, useEffect, useState } from "react"
 
 export const DasbordPages = () => {
+    const [getContent , setgetContent] = useState()
+    const getToken = localStorage.getItem('uL_')
+    const {UserInfo} = useContext(AuthContext)
+
+    useEffect(() => {
+        if(!getToken || !UserInfo){
+            setgetContent(false)
+        }else {
+            setgetContent(true)
+        }
+    },[getToken,UserInfo])
+
     return(
         <>
-        <h1>tester</h1>
+       {
+        getContent ?  <h1>tester</h1> : <div><h1>Anda Tidak Punya Akses</h1></div>
+       }
         </>
     )
 }
