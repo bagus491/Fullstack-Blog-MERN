d7fde79860d3a4b138774319bf917321acc8049d
bagus491 <baguskalam491@gmail.com>
2023-07-30 15:20:59 +0700

    add&setup&fetching(Addpost) ==> frontend add spinner and fetching

--- frontend/src/Components/Sub-Compo/AddPostCompo.jsx
+++ frontend/src/Components/Sub-Compo/AddPostCompo.jsx
@@ -1,19 +1,49 @@
 import { useEffect, useState } from "react"
 import {Spinner} from 'react-bootstrap'
 import {Navigation} from '../Reusebale/Navigation'
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 export const AddPostCompo = () => {
-    const [getSpinner, setgetSpinner] = useState()
+    const [getSpinner, setgetSpinner] = useState(false)
     const getToken = localStorage.getItem('uL_')
     const {Username} = useParams()
-    
+    const Navigate = useNavigate()
     useEffect(() => {
-
-    },[getToken])
+       const getData = async () => {
+        try{
+            const respone = await fetch(`http://localhost:5000/addpost/${Username}`,{
+                method: 'GET',
+                headers: {
+                    'Authorization': getToken
+                }
+            })
+            if(respone.ok){
+                if(respone.status === 200){
+                    setTimeout(() => {
+                        setgetSpinner(true)
+                    },1000)
+                }
+            }else{    
+                    Navigate('*')
+            }
+        }catch{
+            console.error({msg: 'Error'})
+        }
+       }
+       getData()
+    },[getToken,Username,Navigate])
     return(
         <>
-        <h1>tester</h1>
+      {
+        getSpinner ? 
+        <div>
+            <Navigation />
+        </div>   
+        : 
+        <div className="spinner-HomeCompo">
+        <Spinner animation="grow" variant="info" />
+        </div>
+      }
         </>
     )
 }
