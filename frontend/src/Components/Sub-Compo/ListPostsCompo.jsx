ecb8cfe8a2a2eca3af7e9afe578be8ab3e96f588
bagus491 <baguskalam491@gmail.com>
2023-07-30 15:53:22 +0700

    add&changes(listpostsCompo) ==> frontend add Validasi

--- frontend/src/Components/Sub-Compo/ListPostsCompo.jsx
+++ frontend/src/Components/Sub-Compo/ListPostsCompo.jsx
@@ -1,9 +1,50 @@
-
+import { useEffect, useState } from "react"
+import {useNavigate, useParams} from 'react-router-dom'
+import {Navigation} from '../Reusebale/Navigation'
+import {Spinner} from 'react-bootstrap'
 
 export const ListPostsCompo = () => {
+    const [getSpinner, setgetSpinner] = useState(false)
+    const {Username} = useParams()
+    const getToken = localStorage.getItem('uL_')
+    const Navigate = useNavigate()
+
+    useEffect(() => {   
+        const getData = async () => {
+            try{
+                const respone = await fetch(`http://localhost:5000/listposts/${Username}`,{
+                    method: 'GET',
+                    headers: {
+                        'Authorization': getToken
+                    }
+                })
+                if(respone.ok){
+                    if(respone.status === 200){
+                        setTimeout(() => {
+                            setgetSpinner(true)
+                        },1000)
+                    }
+                }else{
+                    Navigate('*')
+                }
+            }catch{
+                console.error({msg: 'Error'})
+            }
+        }
+        getData()
+    },[getToken,Username,Navigate])
     return(
         <>
-        <h1>tester</h1>
+      {
+        getSpinner ? 
+        <div>
+            <Navigation />
+        </div>
+        :
+        <div className="spinner-HomeCompo">
+        <Spinner animation="grow" variant="info" />
+        </div>
+      }
         </>
     )
 }
